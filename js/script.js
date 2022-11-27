02410a682f47cbfbc8ac8ea4b27739ab13084155
Daniil Rodionov <zenitpv@icloud.com>
2022-11-27 21:39:14 +0300

    Update script.js
    add promise version form and slider

--- js/script.js
+++ js/script.js
@@ -121,31 +121,124 @@ window.addEventListener('DOMContentLoaded', function () {
         statusMessage = document.createElement('div');
 
     statusMessage.classList.add('status');
-    
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        form.appendChild(statusMessage);
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-        let formData = new FormData(form);
-        request.send(formData);
+    // without Promise
+    // form.addEventListener('submit', function(event) {
+    //     event.preventDefault();
+    //     form.appendChild(statusMessage);
 
-        request.addEventListener('readystatechange', function() {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
-            }
+    //     let request = new XMLHttpRequest();
+    //     request.open('POST', 'server.php');
+    //     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    //     let formData = new FormData(form);
+    //     request.send(formData);
+
+    //     request.addEventListener('readystatechange', function() {
+    //         if (request.readyState < 4) {
+    //             statusMessage.innerHTML = message.loading;
+    //         } else if (request.readyState === 4 && request.status == 200) {
+    //             statusMessage.innerHTML = message.success;
+    //         } else {
+    //             statusMessage.innerHTML = message.failure;
+    //         }
+    //     });
+
+    //     for (let i = 0; i < input.length; i++) {
+    //         input[i].value = '';
+    //     }
+    // });
+
+        form.addEventListener('submit', function(elem) {
+            elem.preventDefault();
+                form.appendChild(statusMessage);
+                let formData = new FormData(form);
+
+                function postData(data) {
+                    return new Promise(function(resolve, reject) {
+                        let request = new XMLHttpRequest();
+
+                        request.open('POST', 'server.php');
+
+                        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+                        request.addEventListener('readystatechange', function() {
+                                    if (request.readyState < 4) {
+                                        resolve()
+                                    } else if (request.readyState == 4 && request.status == 200) {
+                                        resolve()
+                                    } else {
+                                        reject()
+                                    }
+                                });
+
+                        request.send(data);
+                    })
+                } //End postData
+
+                function clearInput() {
+                    for (let i = 0; i < input.length; i++) {
+                                input[i].value = '';
+                    }
+                }
+
+                postData(formData)
+                    .then(() => statusMessage.innerHTML = message.loading)
+                    .then(() => statusMessage.innerHTML = message.success)
+                    .catch(() => statusMessage.innerHTML = message.failure)
+                    .then(clearInput)
         });
 
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
-        }
-    });
+        //Slider
 
+        let slideIndex = 1,
+            slides = document.querySelectorAll('.slider-item'),
+            prev = document.querySelector('.prev'),
+            next = document.querySelector('.next'),
+            dotsWrap = document.querySelector('.slider-dots'),
+            dots = document.querySelectorAll('.dot');
+
+        showSlides(slideIndex);
+
+        function showSlides(n) {
+
+            if (n > slides.length) {
+                slideIndex = 1;
+            }
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
+            
+            slides.forEach((item) => item.style.display = 'none');
+            dots.forEach((item) => item.classList.remove('dot-active'));
+
+            slides[slideIndex - 1].style.display = 'block';
+            dots[slideIndex - 1].classList.add('dot-active');
+        }
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+        
+        function currentSlide(n) {
+            showSlides(slideIndex = n)
+        }
+
+        prev.addEventListener('click', function() {
+            plusSlides(-1)
+        });
+
+        next.addEventListener('click', function() {
+            plusSlides(1)
+        });
+
+
+        dotsWrap.addEventListener('click', function(event) {
+            for (let i = 0; i < dots.length + 1; i++) {
+                if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                    currentSlide(i);
+                }
+            }
+        });
 });
